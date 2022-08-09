9d289444976431602147b237c6374d3b0c104cfd
Andressa Oliveira <andressa_barros18@hotmail.com>
2022-08-09 15:05:07 +0000

    atualização api node

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello, Andressa");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('servidor Andressa no ar...')
+})
+
+app.listen(3000)
+app.get("/oi", function (req, res) {
+    res.send("Olá, mundo!");
+});
+app.get("/dia", function (req, res) {
+    res.send("um dia triste nublado");
+})
